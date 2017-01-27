5e9782e009bca99eb4559c04cf6137a654ccf548
Dillon Corkran <dcorkran972@gmail.com>
2017-01-27 15:06:04 -0700

    :+1: yay

--- client/app/game/game.controller.js
+++ client/app/game/game.controller.js
@@ -1,11 +1,32 @@
 angular
   .module('app')
-  .controller('GameController',function(){
+  .controller('GameController',function($sce){
     const vm = this;
 
     vm.$onInit = function(){
-      console.log('hey');
-      //  vm.currentNavItem = 'page1';
+
+    },
+    vm.startGame= function(){
+      vm.levelOne();
+    },
+    vm.levelOne=function(){
+      vm.ap = `https://my.vetmatrixbase.com/clients/12679/images/cats-animals-grass-kittens--800x960.jpg`;
+      vm.qp1 = $sce.trustAsHtml(`<img ng-click="$ctrl.loseGame()" src="http://www.dreamdictionarynow.com/wp-content/uploads/2014/08/weird-facts-about-dreams.jpg">`);
+      vm.qp2 = $sce.trustAsHtml(`<img ng-click="$ctrl.winGame()" src="https://my.vetmatrixbase.com/clients/12679/images/cats-animals-grass-kittens--800x960.jpg">`);
+      // vm.qp3 =
+      // vm.qp4 =
+    },
+    vm.levelTwo=function(){
+      vm.ap = `http://img1.izismile.com/img/img3/20100725/640/dreadful_and_weird_640_15.jpg`;
+      vm.qp1 = $sce.trustAsHtml(`<img ng-click="$ctrl.loseGame()" src="http://www.dreamdictionarynow.com/wp-content/uploads/2014/08/weird-facts-about-dreams.jpg">`);
+      vm.qp2 = $sce.trustAsHtml(`<img ng-click="$ctrl.winGame()" src="http://img1.izismile.com/img/img3/20100725/640/dreadful_and_weird_640_15.jpg">`);
+    }
+    vm.winGame=function(){
+
+      console.log('hi');
+    },
+    vm.loseGame=function(){
+      console.log('asdasd');
     }
 
   })
